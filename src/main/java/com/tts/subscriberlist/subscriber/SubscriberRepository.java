a2722bc18d1e06bbb63fc4cbf19190ba53fb4b3e
soflybird <74286728+soflybird@users.noreply.github.com>
2021-02-05 14:25:07 -0600

    Initial commit

---
+++ src/main/java/com/tts/subscriberlist/subscriber/SubscriberRepository.java
@@ -0,0 +1,11 @@
+package com.tts.subscriberlist.subscriber;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
+
+
+
+
+}
